b29ddd3ab6a74cab757b062d01ccb3314cd4378d
julian soro <juliansoro.dev@gmail.com>
2026-09-25 13:50:27 -0400

    slight theme adjust colors

--- lib/map-palette.ts
+++ lib/map-palette.ts
@@ -2,7 +2,7 @@ export const mapPalette = {
   background: '#303236',
   landcover: '#36383c',
   park: '#3a3c40',
-  building: '#3e4145',
+  building: '#36383c',
   water: '#252b31',
   waterway: '#171d22',
   road: '#171b1f',
